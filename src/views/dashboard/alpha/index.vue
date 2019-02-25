b61fe8f097d8b20cebbf4877e24a1749e1a2c493
Frontend <hello@mediatec.org>
2019-02-25 11:15:40 +0300

    example component

--- src/views/dashboard/alpha/index.vue
+++ src/views/dashboard/alpha/index.vue
@@ -5,5 +5,14 @@
     <a-button>Default</a-button>
     <a-button type="dashed">Dashed</a-button>
     <a-button type="danger">Danger</a-button>
+    <cui-chart-card />
   </div>
 </template>
+
+<script>
+import CuiChartCard from '@/components/CleanUIComponents/ChartCard'
+
+export default {
+  components: { CuiChartCard },
+}
+</script>
