ac7bb140b912a1e2a1d170c17c8307c099e0b499
Frontend <hello@mediatec.org>
2019-03-13 22:45:05 +0300

    setup settings from url

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,14 @@
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'app',
+  watch: {
+    '$route' (to, from) {
+      this.$store.commit('SETUP_URL_SETTINGS', to.query)
+    },
+  },
+}
+</script>
--- src/store/settings/index.js
+++ src/store/settings/index.js
@@ -33,6 +33,24 @@ export default {
       window.localStorage.setItem(`app.settings.${payload.setting}`, payload.value)
       state[payload.setting] = payload.value
     },
+    SETUP_URL_SETTINGS(state, payload) {
+      let queryParams = payload
+      let keys = false
+      if (payload.redirect) {
+        const str = payload.redirect
+        const subs = str.substring(str.indexOf('?') + 1)
+        if (str.indexOf('?') >= 0) {
+          queryParams = JSON.parse('{"' + decodeURI(subs).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}')
+        }
+      }
+      delete queryParams['redirect']
+      keys = Object.keys(queryParams)
+      if (keys.length) {
+        keys.forEach(key => {
+          if (key in state) { state[key] = queryParams[key] === 'true' }
+        })
+      }
+    },
   },
   actions: {},
   getters: {
