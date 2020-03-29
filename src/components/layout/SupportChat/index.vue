34b4bf024e1a615b231ce589c1acdb5026f14f1a
Mediatec Software <hello@mediatec.org>
2020-03-29 15:07:24 +0300

    support chat color fix

--- src/components/layout/SupportChat/index.vue
+++ src/components/layout/SupportChat/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="$style.chat">
     <button type="button" :class="$style.toggleButton" @click="toggleSupportChat()">
-      <i :class="$style.icon" class="fe fe-message-square text-blue mr-md-2" />
+      <i :class="$style.icon" class="fe fe-message-square mr-md-2" />
       <span class="d-none d-md-inline">Support Chat</span>
     </button>
     <div :class="{[$style.container]: true, [$style.containerToggled]: settings.isSupportChatOpen}">
